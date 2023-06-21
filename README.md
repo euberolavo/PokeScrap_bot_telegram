b70d39b94fd54e89a803b60bb2e7ca5e45e0073f
Euber Santos <euber.olavo.bezerra@gmail.com>
2023-06-20 22:44:21 -0300

    Finalizando Bot

---
+++ README.md
@@ -0,0 +1,64 @@
+PokeScraper
+# PokeScraper Chatbot
+
+_Chatbot desenvolvido para uso do [Web Scraper pokemon](https://github.com/euberolavo/Scraper_Pokemon_Puppeteer) que está implementado no AWS Lambda_
+
+***********
+### Tópicos 
+
+- [Sobre o projeto](#Sobre-o-projeto)
+
+- [Tecnologias](#Tecnologias)
+
+- [Instalação](#Instalação)
+
+- [Funcionamento](#Funcionamento)
+
+- [Desenvolvimento](#Desenvolvimento)
+
+- [Considerações finais](#Considerações-finais)
+
+***********
+## Sobre o projeto
+
+### O projeto foi desenvolvido em NodeJS, com uso da Biblioteca telegraf
+
+- O bot está implementado em uma VPS Oracle;
+- Ele recebe um nome de Pokemon, e retorna os dados obtidos pelo [PokeScraper](Scraper_Pokemon_Puppeteer) implementado na AWS Lambda;
+
+## Tecnologias
+* NodeJS
+* telegraf
+
+## Instalação
+Clonar o projeto:
+```bash
+git clone https://github.com/euberolavo/PokeScrap_bot_telegram.git
+```
+Baixar as dependências:
+```bash
+npm install
+```
+Startar o Bot
+```bash
+npm start
+```
+## Funcionamento
+
+###Local
+- Ao iniciar o Bot, ele retornará no terminal 
+```bash
+PokeScraper Telegram em Funcionamento
+```
+Após isso, é apenas abrir o [PokeScraper](https://t.me/PokeScraperBot) no telegram, e enviar "/start" para iniciar a conversa.
+
+*****
+
+## Desenvolvimento
+No processo de desenvolvimento do Scraper, senti a necessidade de criar algum front para interação do usuario, então tive a ideia de criar algo mais interativo, e surgiu o Bot.
+
+Esse foi o meu primeiro contato com o telegraf, e devido a documentação ser bastate detalhada, não tive dificuldades no desenvolvimento.
+
+## Considerações finais
+
+Sei que poderia implementar o bot no AWS, e pretendo realiza-lo, mas devido o tempo, decidi implementar em uma VPS ao qual possuo.
---
+++ index.js
@@ -0,0 +1,61 @@
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+
+class Bot {
+  constructor(token) {
+    this.bot = new Telegraf(token);
+    this.bot.start(this.startHandler.bind(this));
+    this.bot.on('text', this.textHandler.bind(this));
+    this.bot.launch();
+    this.nome = '';
+    console.log('PokeScraper Telegram em Funcionamento');
+  }
+
+  startHandler(ctx) {
+    const chatId = ctx.chat.id;
+    this.bot.telegram.sendMessage(chatId, 'Olá, bem-vindo ao PokeScraper!');
+    setTimeout(() => {
+      ctx.reply(`Digite o nome do seu Pokémon Preferido ❤️`);
+    }, 1000);
+  }
+
+  async textHandler(ctx) {
+    const nome = ctx.message.text;
+    this.replyWithNome(ctx, nome);
+    this.nome = nome;
+
+    try {
+      const response = await axios.get(
+        `https://worb0n0l5a.execute-api.us-east-1.amazonaws.com/dev/?pokemon=${nome}`
+      );
+
+      // Processar a resposta da API
+      const pokemonData = response.data.body;
+
+      // Exemplo de resposta ao usuário
+      ctx.reply(
+        `Nome: ${pokemonData.name}\nPeso: ${pokemonData.peso},\nAltura: ${
+          pokemonData.altura
+        },\nTipo: ${pokemonData.types.join(
+          ', '
+        )},\nHabilidades: ${pokemonData.habilidades.join(', ')}\n`
+      );
+      console.log(pokemonData);
+      // ctx.reply(`Tipo(s) do Pokémon: ${pokemonData.types.join(', ')}`);
+      // // ... adicione outras respostas conforme necessário
+    } catch (error) {
+      console.log('Erro ao fazer a solicitação à API:', error);
+      ctx.reply('Ocorreu um erro ao obter informações do Pokémon.');
+    }
+  }
+
+  replyWithNome(ctx, nome) {
+    ctx.reply(`Você digitou ${nome}`);
+    setTimeout(() => {
+      ctx.reply(`Aguarde enquanto coletamos informações`);
+    }, 1000);
+  }
+}
+
+const bot = new Bot('5943687601:AAHIBuOeHTMkq86m1wVvJRVGav_3V8wHJ00');
+module.exports = bot;
